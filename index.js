5cd3a6401f4842b9f0c120e442ae93567ab52083
dominic-taylor <dataylorx@gmail.com>
2018-04-29 11:47:20 +1200

    Fixed dropdown menus from covering content when fullscreen nav closed

--- index.js
+++ index.js
@@ -1,25 +1,45 @@
 
-let navDropdown = document.getElementsByClassName('nav-dropdown-btn');
 
 document.getElementById('menu-icon').addEventListener('click', function() {
-	toggleFullScreenNav()
+	toggleFullScreenNav();
 });
 
 document.getElementById('close-fullscreen').addEventListener('click', function() {
-	toggleFullScreenNav()
+	toggleFullScreenNav();
 });
 
+let navDropdown = document.getElementsByClassName('nav-dropdown-btn');
+
 for (var i = navDropdown.length - 1; i >= 0; i--) {
-	navDropdown[i].addEventListener('click', toggleDropDown, false)
+	navDropdown[i].addEventListener('click', toggleDropDown, false);
 }
 
 
 function toggleDropDown(e) {
-	e.target.nextElementSibling.classList.toggle('nav-links-open')
+	e.target.nextElementSibling.classList.toggle('nav-links-open');
 }
 
 function toggleFullScreenNav(){
 	let nav = document.getElementsByClassName('nav-menu')[0];
-	nav.classList.toggle('fullscreen')
+	// nav.classList.toggle('fullscreen');
 
+	if (nav.classList.contains('fullscreen')) {
+		let navLinks = document.getElementsByClassName('nav-links');
+		for (var i = navLinks.length - 1; i >= 0; i--) {
+			if(navLinks[i].classList.contains('nav-links-open')){
+				navLinks[i].classList.remove('nav-links-open');
+			}
+		}
+		nav.classList.remove('fullscreen');
+	}else{
+		nav.classList.add('fullscreen');
+	}
+
+
+	// if nav is open{
+	// 	if(one of the navlinks is open) close it
+	// 	then close nav
+	// }else{
+	// 	open nav fs
+	// }	
 }
